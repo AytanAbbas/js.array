ef0678a8af84a049ad5df89b11e193f45363aeac
AytanAbbas <87373585+AytanAbbas@users.noreply.github.com>
2021-10-13 01:40:08 +0400

    Add files via upload

---
+++ js tasks/js/style.js
@@ -0,0 +1,23 @@
+// class Student {
+//     constructor(Name) {
+//         this.name = Name;
+//     }
+// }
+// let getStudentNames = (string) => {
+//     return string.name
+// }
+// let student1 = new Student(["Steve", "Mike", "John"]);
+// console.log(getStudentNames(student1));
+
+
+
+// let cityFacts = (object) => {
+//     let result = `${object['name']} has a population of ${object['population']} and is situated in ${object['continent']}`
+//     return result
+// }
+// let city = {
+//     name: "Paris",
+//     population: "2,140,526",
+//     continent: "Europe"
+// }
+// console.log(cityFacts(city))
